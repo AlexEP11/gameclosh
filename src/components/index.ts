f33fab04c4d92be9a65f41753c9b349de5ecb92b
AlexEP11 <aleeps2911@gmail.com>
2025-05-03 09:46:53 -0600

    Agrega rutas nuevas en el archivo de barril

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export * from "./ui/hero/Hero";
 export * from "./ui/navigation/Navigation";
+export * from "./ui/auth-button/AuthButton";
+export * from "./ui/search-input/SearchInput";
 
 export * from "./games/games-grid/GamesGrid";
 export * from "./games/games-grid/GamesGridItem";
@@ -7,3 +9,7 @@ export * from "./games/games-grid/GamesGridItem";
 export * from "./game/game-info/GamePreview";
 export * from "./game/game-info/GameDescription";
 export * from "./game/game-info/GamePlatforms";
+
+export * from "./auth/countdown/Countdown";
+export * from "./auth/auth-message/AuthMessage";
+export * from "./auth/auth-action/AuthAction";
