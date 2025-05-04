ef896cbad053dd055493fcabc3309ec6b3e3d54e
AlexEP11 <aleeps2911@gmail.com>
2025-05-04 10:00:18 -0600

    Agrega nuevos componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,7 +9,14 @@ export * from "./games/games-grid/GamesGridItem";
 export * from "./game/game-info/GamePreview";
 export * from "./game/game-info/GameDescription";
 export * from "./game/game-info/GamePlatforms";
+export * from "./game/game-reviews/GameReviews";
+export * from "./game/game-reviews/GameReview";
+export * from "./game/game-reviews/GameReviewInput";
 
 export * from "./auth/countdown/Countdown";
 export * from "./auth/auth-message/AuthMessage";
 export * from "./auth/auth-action/AuthAction";
+
+export * from "./reviews/review-grid/ReviewGridItem";
+export * from "./reviews/review-modal/ReviewModalDelete";
+export * from "./reviews/review-modal/ReviewModalPut";
